6076d0e55167ec4a1cdd3a0b54f9dee50d83d2e8
AJITH1113 <ajiajith1113@gmail.com>
2024-12-05 08:34:01 +0530

    changed status

---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Weather.css";
+const Weather = () => {
+  return (
+    <div className="Weather">
+      <div className="search-bar">
+        <input type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
